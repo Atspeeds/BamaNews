e58e5caab2a6ae8e0d16eae1d74f179342aeb6a6
AmirSam <108144259+IRanGroup@users.noreply.github.com>
2022-08-08 09:51:42 -0700

    Transfer template to project

--- BamaNews.Presentation.MvcCore/Pages/Index.cshtml.cs
+++ BamaNews.Presentation.MvcCore/Pages/Index.cshtml.cs
@@ -1,28 +1,16 @@
-﻿using BN.Application.Contract.NewsCategory;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BamaNews.Presentation.MvcCore.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly INewsCategoryApplication _NewsCategory;
-
-        public IndexModel(INewsCategoryApplication newsCategory)
-        {
-            _NewsCategory = newsCategory;
-        }
-
-        public List<NewsCategoryViewModel> NewsCategoriesModel;
-
         public void OnGet()
         {
-            NewsCategoriesModel = _NewsCategory.SelectAll();
         }
     }
 }
