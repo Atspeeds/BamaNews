a19e6be0943107c7d38f92f2f49198cfc60d206b
AmirSam <irantehranid@yahoo.com>
2022-09-10 06:01:34 -0700

    Small changes in comments and comments

--- BN.Infrastrure.EFCore/Repository/CommentRepository.cs
+++ BN.Infrastrure.EFCore/Repository/CommentRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BN.Application.Contract.Comment;
 using BN.Domain.CommentAgg;
+using BN.Infrastrure.Query;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,12 +30,12 @@ namespace BN.Infrastrure.EFCore.Repository
                     Title = x.TitleComment,
                     Message = x.Message,
                     StatusComment = x.StatusComment,
-                    CreationDate = x.Creationdate.ToString(),
+                    CreationDate = x.Creationdate.Toshamsi(),
                     UserName = x.Users.UserName,
                     NewsTitle = x.News.NewsTitle,
                     UserId=x.UserId,
                     NewsId=x.NewsId
-                }).AsNoTracking().OrderBy(z => z.Id).ToList();
+                }).AsNoTracking().OrderBy(z => z.StatusComment).ToList();
         }
 
         public void Save()
